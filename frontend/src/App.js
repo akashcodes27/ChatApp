7d90321c1a3d7045ca4b43be5f4c61304076792f
Akash Patil <akashpatil2002@gmail.com>
2024-07-04 14:13:13 +0530

    creating home login signup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,11 @@
 
 function App() {
   return (
-    <>
+    <div className='p-4 h-screen flex items-center justify-center border-4 border-black'>
       <h1>This works</h1>
-      <button className="btn btn-primary">Primary</button>
+      {/* <button className="btn btn-primary">Primary</button> */}
 
-    </>
+    </div>
   );
 }
 
