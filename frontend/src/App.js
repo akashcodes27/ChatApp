8612547fb21824e3d01f46771185865ef8bcdf87
Akash Patil <akashpatil2002@gmail.com>
2024-07-04 13:15:01 +0530

    Installed tailwind and daisyUI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,8 +4,15 @@ function App() {
   return (
     <>
       <h1>This works</h1>
+      <button className="btn btn-primary">Primary</button>
+
     </>
   );
 }
 
 export default App;
+
+
+
+
+// we have installed tailwindcss, we have also installed daisyUI to simplify tailwind, we have installed an extension called "Tailwind CSS IntelliSense" to see all the css properties that are present inside any daisyUI class
